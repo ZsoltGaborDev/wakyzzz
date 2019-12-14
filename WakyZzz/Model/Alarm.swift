b1d687f6fece07b1960621bc0e99ee9c6b021e4c
zsolt13 <zsolt@bereadysoftware>
2019-12-14 02:12:55 +0100

    TODO: realm to be tested

--- WakyZzz/Model/Alarm.swift
+++ WakyZzz/Model/Alarm.swift
@@ -16,7 +16,7 @@ class Alarm: Object {
     
     static let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     
-    var time = Int()
+    var time = 8 * 3600
     var repeatDays = [false, false, false, false, false, false, false]
     var enabled = true
     
